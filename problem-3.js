df3fccf85581ad06403ae0b76ec3e7980693e9a7
bdahsan1999wd <bdahsan1999wd@gmail.com>
2023-02-25 13:53:18 +0600

    problem-3 added

---
+++ problem-3.js
@@ -0,0 +1,40 @@
+/*
+
+How will you get the output
+
+a) abc
+
+b) xyz
+
+*/
+
+let activities = {
+    activity1: [
+        {
+            name: "listen on spotify",
+            song_list: {
+                song_1: "abc",
+                song_2: "bcd"
+            },
+            id: 1
+        },
+        {
+            name: "listen music through bot",
+            song_list: {
+                song_1: "wxy",
+                song_2: "xyz"
+            },
+            id: 2
+        }
+    ]
+};
+
+
+// To get "abc":
+let song1 = activities.activity1[0].song_list.song_1;
+console.log(song1);
+
+
+// To get "xyz":
+let song2 = activities.activity1[1].song_list.song_2;
+console.log(song2);
