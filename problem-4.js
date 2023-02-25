2fc97d6be7d817769bcf07832f54efad5743d213
bdahsan1999wd <bdahsan1999wd@gmail.com>
2023-02-25 13:54:03 +0600

    problem-4 added

---
+++ problem-4.js
@@ -0,0 +1,40 @@
+/* 
+
+How will you display
+
+a) Petersburg
+
+b) Harry
+
+*/
+
+let students = {
+    2222: {
+        name: "Jack",
+        section: "C",
+        class: "IX",
+        address: {
+            "building no": 12,
+            "street": "St. Jonson",
+            "city": "Petersburg",
+            "country": "UK"
+        }
+    },
+    3333: {
+        name: "Harry",
+        section: "D",
+        class: "X",
+        address: {
+            "building no": 85,
+            "street": "DC road",
+            "city": "Kachukhet",
+            "country": "Bangladesh"
+        }
+    }
+};
+
+// display "Petersburg"
+console.log(students[2222].address.city);
+
+// display "Harry"
+console.log(students[3333].name);
